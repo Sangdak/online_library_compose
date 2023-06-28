017c73b6ad972042db7e05d0e7df876bf489edf9
Sangdak <sh.doppelganger@gmail.com>
2023-06-29 09:47:02 +1000

    Split books list by pages.

--- main.py
+++ main.py
@@ -3,26 +3,40 @@ from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from more_itertools import chunked
+import os
+from pathlib import Path
+
+
+BOOKS_ON_PAGE_AMOUNT = 20
 
 
 def on_reload():
     books_db = get_books_data()
     book_db = [{k: v} for k, v in books_db.items()]
 
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
+    os.makedirs('pages', exist_ok=True)
 
-    template = env.get_template('template.html')
+    books_divided_by_pages = chunked(book_db, BOOKS_ON_PAGE_AMOUNT)
 
-    rendered_page = template.render(
-        books=chunked(book_db, 2),
-    )
+    print(books_divided_by_pages)
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    print("Content refreshed")
+    for page_number, page_content in enumerate(books_divided_by_pages, start=1):
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+
+        template = env.get_template('template.html')
+
+        rendered_page = template.render(
+            books=chunked(page_content, 2),
+        )
+
+        filepath = Path('pages', f'index{page_number}.html')
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+        print("Content refreshed")
 
 
 def get_books_data():
